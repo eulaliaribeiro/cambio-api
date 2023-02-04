65f9ca137c27643ff8436de7f95a3a9ac46bdff5
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-02-04 04:29:48 -0300

    primeiro commit

---
+++ src/test/java/com/ada/deva/cambioApi/CambioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.deva.cambioApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
