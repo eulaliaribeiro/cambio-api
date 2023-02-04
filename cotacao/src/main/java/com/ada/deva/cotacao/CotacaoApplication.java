52a2dfba1a7dee3bc0f3f979893eff3a1f2cc1a4
BrunaMassuchini <brunaccelestino@gmail.com>
2023-02-04 13:13:51 -0300

    cotacao

---
+++ cotacao/src/main/java/com/ada/deva/cotacao/CotacaoApplication.java
@@ -0,0 +1,12 @@
+package com.ada.deva.cotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotacaoApplication.class, args);
+	}
+
+}
---
+++ cotacao/src/test/java/com/ada/deva/cotacao/CotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.deva.cotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
