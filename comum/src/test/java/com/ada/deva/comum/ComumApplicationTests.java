b75fe6d70040bcf330efe62cad8ab073a000ee98
Ana Thais Senger <anathaissenger@hotmail.com>
2023-02-05 17:21:49 -0300

    exception de entidade duplicada

---
+++ comum/src/test/java/com/ada/deva/comum/ComumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.deva.comum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
