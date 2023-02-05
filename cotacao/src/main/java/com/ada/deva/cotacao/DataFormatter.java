d83b6a9d87da312dea9ab9e8b35ac890f798d136
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-02-05 20:01:24 -0300

    adicionando mensagem de erro no formatter

--- cotacao/src/main/java/com/ada/deva/cotacao/DataFormatter.java
+++ cotacao/src/main/java/com/ada/deva/cotacao/DataFormatter.java
@@ -8,12 +8,11 @@ public class DataFormatter {
     public static LocalDateTime dataFormatter(String date) {
         date = date.replace(" ", "T");
 
-
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
             return LocalDateTime.parse(date, formatter);
         } catch(DateTimeParseException e) {
-
+            System.err.println("Formato de data inválido! Digite como dd/mm/aaaa");
         }
 
         return  null;
